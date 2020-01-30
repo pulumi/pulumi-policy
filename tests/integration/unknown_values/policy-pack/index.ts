15be8876ea2a9290cd8a7f15d739ca0b7707044c
Justin Van Patten <jvp@justinvp.com>
2020-01-29 17:26:16 -0800

    Report violation when accessing unknowns during stack validation
    For resource validations, an advisory violation is reported when accessing unknown values. We weren't doing this for stack validations.
    
    This change fixes it for stack validations, and adds an integration test that ensures we're doing it for both resource and stack validations.

---
+++ tests/integration/unknown_values/policy-pack/index.ts
@@ -0,0 +1,32 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+import { PolicyPack, validateStackResourcesOfType, validateResourceOfType } from "@pulumi/policy";
+
+import * as random from "@pulumi/random";
+
+new PolicyPack("unknown-values-policy", {
+    policies: [
+        {
+            name: "unknown-values-resource-validation",
+            description: "Accessing unknown values during preview results in a violation.",
+            enforcementLevel: "mandatory",
+            validateResource: validateResourceOfType(random.RandomPet, (pet, args, reportViolation) => {
+                // Accessing `.prefix` is expected to result in a policy violation because its value is unknown
+                // during previews given the associated Pulumi program.
+                console.log(pet.prefix);
+            }),
+        },
+        {
+            name: "unknown-values-stack-validation",
+            description: "Accessing unknown values during preview results in a violation.",
+            enforcementLevel: "mandatory",
+            validateStack: validateStackResourcesOfType(random.RandomPet, (pets, args, reportViolation) => {
+                for (const pet of pets) {
+                    // Accessing `.prefix` is expected to result in a policy violation because its value is unknown
+                    // during previews given the associated Pulumi program.
+                    console.log(pet.prefix);
+                }
+            }),
+        },
+    ],
+});
