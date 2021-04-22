602b09b8fbd6d846562827c18120e644aa1d9b05
Justin Van Patten <jvp@justinvp.com>
2021-04-22 10:02:09 -0700

    Update provider test

--- tests/integration/provider/policy-pack/index.ts
+++ tests/integration/provider/policy-pack/index.ts
@@ -47,10 +47,10 @@ function validate(r: ResourceValidationArgs | PolicyResource) {
         case "random:index/randomUuid:RandomUuid":
             assert.notStrictEqual(r.provider, undefined);
             assert.strictEqual(r.provider!.type, "pulumi:providers:random");
-            assert.strictEqual(r.provider!.name, "default_2_0_0");
-            assert.strictEqual(r.provider!.urn, createURN("pulumi:providers:random", "default_2_0_0"));
+            assert.strictEqual(r.provider!.name, "default_4_0_0");
+            assert.strictEqual(r.provider!.urn, createURN("pulumi:providers:random", "default_4_0_0"));
             assert.notStrictEqual(r.provider!.props, undefined);
-            assert.deepStrictEqual(r.provider!.props.version, "2.0.0");
+            assert.deepStrictEqual(r.provider!.props.version, "4.0.0");
             break;
 
         case "random:index/randomString:RandomString":
@@ -59,7 +59,7 @@ function validate(r: ResourceValidationArgs | PolicyResource) {
             assert.strictEqual(r.provider!.name, "my-provider");
             assert.strictEqual(r.provider!.urn, createURN("pulumi:providers:random", "my-provider"));
             assert.notStrictEqual(r.provider!.props, undefined);
-            assert.deepStrictEqual(r.provider!.props.version, "2.0.0");
+            assert.deepStrictEqual(r.provider!.props.version, "4.0.0");
             break;
 
         default:
